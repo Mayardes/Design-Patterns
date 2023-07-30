acc8cac3782f83890c79a6e2d76f27bbf73b8203
Mayardes Oliveira <mayardesoliveira@gmail.com>
2023-07-30 12:27:25 -0300

    initial commit

---
+++ S.O.L.I.D/Journal.cs
@@ -0,0 +1,10 @@
+﻿namespace S.O.L.I.D
+{
+    internal class Journal
+    {
+        private readonly List<string> _entries = new List<string>();
+
+        //just a counter for total # entries.
+        private static int count = 0;
+    }
+}
